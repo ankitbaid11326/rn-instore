082a7f34aa88696455bed02ad933d45d5664cd65
Ankit Baid <ankitbaid11326@gmail.com>
2019-01-29 17:41:05 +0530

    Google Login iOS hotfix

--- src/api/google.js
+++ src/api/google.js
@@ -1,42 +1,26 @@
 import { Google, Constants } from 'expo';
 
-// const scopes = ['profile', 'email'];
-
-// const loginAsync = async () => {
-//   try {
-//     const result = await Google.logInAsync({
-//       androidClientId: '891645207122-0bb4n8r99hc5unpbm10lq000hgmekuvh.apps.googleusercontent.com',
-//       iosClientId: '891645207122-5emhsbcrgov3gg8cm9sdppvbs0s399e1.apps.googleusercontent.com'
-//     });
-
-//     if (result.type === 'success') {
-//       return Promise.resolve(result.accessToken);
-//     }
-
-//     return Promise.reject('No Success');
-//   } catch (error) {
-//     return Promise.reject(error);
-//   }
-// };
+const scopes = ['profile', 'email'];
 
 const loginAsync = async () => {
-  try {
-    const result = await Google.logInAsync({
-      androidClientId: '891645207122-75se6gfsep4n3udvqpohqn4m8t3s7iu0.apps.googleusercontent.com',
-      iosClientId: '891645207122-5emhsbcrgov3gg8cm9sdppvbs0s399e1.apps.googleusercontent.com',
-      scopes: ['profile', 'email']
-    });
+    try {
+        const result = await Google.logInAsync({
+            androidClientId: '891645207122-0bb4n8r99hc5unpbm10lq000hgmekuvh.apps.googleusercontent.com',
+            iosClientId: '891645207122-5emhsbcrgov3gg8cm9sdppvbs0s399e1.apps.googleusercontent.com',
+            scopes,
+            behavior: 'web',
+        });
 
-    if (result.type == 'success') {
-      return Promise.resolve(result.accessToken);
-    } else {
-      return Promise.reject('Filed');
+        if (result.type === 'success') {
+            return Promise.resolve(result.accessToken);
+        }
+
+        return Promise.reject('No Success');
+    } catch (error) {
+        return Promise.reject(error);
     }
-  } catch (error) {
-    return Promise.reject(error);
-  }
 };
 
 export const GoogleApi = {
-  loginAsync: loginAsync
+    loginAsync: loginAsync
 };
